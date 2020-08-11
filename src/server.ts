8e9847bfe3d0521920423a6e4122a2ece238e4a9
andrewweaver <andyweaver@me.com>
2020-08-10 21:06:05 -0400

    add body parser

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
 import {sendGuildApplicationToDiscord, sendGuildComplaintToDiscord} from './discord'
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 require('dotenv').config()
 
-
 const HTTP_SERVER_PORT = process.env.HTTP_SERVER_PORT || 3000;
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-
 })
-
 
 app.post("/submit-guild-application", async (req, res) => {
     const application = req.body;
