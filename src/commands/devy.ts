91ad0a5feff9acdb600589b16b3f50db277523a6
andrewweaver <andyweaver@me.com>
2020-08-11 20:32:13 -0400

    Add new things

---
+++ src/commands/devy.ts
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'devy',
+    description: 'Devy',
+    execute(message, args) {
+        message.channel.send('Has the weakest moves and looks like a bird.');
+    },
+};
---
+++ src/commands/stream.ts
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'stream',
+    description: 'Twitch Streams',
+    execute(message, args) {
+        message.channel.send('Man it\'s crazy that only one person streams here. <https://twitch.tv/loons>');
+    },
+};
