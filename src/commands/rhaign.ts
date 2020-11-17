2dabdf274b1cb76e8b2e879629a4cab3a0e37bf8
andrewweaver <andyweaver@me.com>
2020-11-16 23:15:27 -0500

    fix command

--- src/commands/rhaign.ts
+++ src/commands/rhaign.ts
@@ -2,6 +2,6 @@ module.exports = {
 	name: ['Rhaign', 'rhaign', 'Mists', 'mists', 'Fullpotato', 'fullpotato' ],
 	description: 'Rhaign',
 	execute(message, args) {
-		message.channel.send('Rhaign wasn\'t invited to join the clique');
+		message.channel.send('Rhaign wasn\'t invited to join the clique.');
 	},
 };
