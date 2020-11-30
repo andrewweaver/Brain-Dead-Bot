0a9aeef8cdf7711bf2d989b3aea1e82c06c67343
andrewweaver <andyweaver@me.com>
2020-11-29 23:28:17 -0500

    add raid bots command

---
+++ src/commands/raidbots.ts
@@ -0,0 +1,10 @@
+module.exports = {
+    name: ['raidbots', 'Raidbots', 'RaidBots'],
+    description: 'Raidbots Account',
+    execute(message, args) {
+        message.channel.send(
+            '**Email:** jpanguild@gmail.com \n' +
+            '**Password:** devyisdumb' 
+        );
+    },
+};
