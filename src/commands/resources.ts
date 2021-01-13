89bd3a1a76b304b3c528ddf04379cb507ecbbd49
Andrew Weaver <andyweaver@me.com>
2021-01-13 11:53:26 -0500

    things

--- src/commands/resources.ts
+++ src/commands/resources.ts
@@ -4,7 +4,8 @@ module.exports = {
     execute(message, args) {
         message.channel.send(
             '**Spreadsheet:** <https://shorturl.at/enzBF> \n' +
-            '**Warcraft Logs:** <https://shorturl.at/aqOX0> \n' +
+            '**Warcraft Logs:** <https://shorturl.at/eALPY> \n' +
+            '**WoW Audit:** <https://shorturl.at/prxQ9> \n' +
             '**Application:** <https://shorturl.at/nxGIX> \n' +
             '**Complaints:** <https://shorturl.at/bdoGM>'
         );
