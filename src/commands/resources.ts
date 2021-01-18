264471725f74d084f4072ed787acd4d27d4f5516
Andrew Weaver <andyweaver@me.com>
2021-01-18 12:34:21 -0500

    stuff

--- src/commands/resources.ts
+++ src/commands/resources.ts
@@ -5,6 +5,7 @@ module.exports = {
         message.channel.send(
             '**Spreadsheet:** <https://shorturl.at/enzBF> \n' +
             '**Warcraft Logs:** <https://shorturl.at/eALPY> \n' +
+            '**Join Warcraft Logs Guild:** <https://shorturl.at/hCKY7> \n' +
             '**WoW Audit:** <https://shorturl.at/prxQ9> \n' +
             '**Application:** <https://shorturl.at/nxGIX> \n' +
             '**Complaints:** <https://shorturl.at/bdoGM>'
