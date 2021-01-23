1d041cfe5626e608fb3a32f82f3960b014eb804a
andrewweaver <andyweaver@me.com>
2021-01-22 21:55:15 -0500

    fix warcraft logs links

--- src/commands/resources.ts
+++ src/commands/resources.ts
@@ -4,8 +4,8 @@ module.exports = {
     execute(message, args) {
         message.channel.send(
             '**Spreadsheet:** <https://shorturl.at/enzBF> \n' +
-            '**Warcraft Logs:** <https://shorturl.at/eALPY> \n' +
-            '**Join Warcraft Logs Guild:** <https://shorturl.at/hCKY7> \n' +
+            '**Warcraft Logs:** <https://www.warcraftlogs.com/guild/id/565626> \n' +
+            '**Join Warcraft Logs Guild:** <https://www.warcraftlogs.com/guild/autojoin/565626/CKzBDr17> \n' +
             '**WoW Audit:** <https://shorturl.at/prxQ9> \n' +
             '**Application:** <https://shorturl.at/nxGIX> \n' +
             '**Complaints:** <https://shorturl.at/bdoGM>'
